9084a5e73a0b0d4d9e91a86a7f6bbbd1d039a225
h_h <hernaultcontact@gmail.com>
2017-12-08 00:51:05 +0100

    Première version du slider en POO

--- js/slider.js
+++ js/slider.js
@@ -1,142 +1,153 @@
-// Tableaux des objets slide
+// Tableau des objets slide
 var slides = [
   {
+    parentDivId : "container-1",
     src: "images/credited/sources/optimise/velib-bleu1.png",
     alt: "Vélib sur les quais",
     title : "Vélib' 2018 - Version électrique",
-    text : "Choisir une station sur la carte",
-    imgId : "current-slide",
-    txtId : "current-text",
-    parentId : "conteneur"
+    text : "Choisir une station sur la carte"
   },
   {
+    parentDivId : "container-1",
     src: "images/credited/sources/optimise/velib-bleu2.png",
     alt: "Vélib sur les quais",
     title : "Vélib' 2018 - Version électrique",
-    text : "Vérifier qu'un vélo est disponible",
-    imgId : "current-slide",
-    txtId : "current-text",
-    parentId : "conteneur"
+    text : "Vérifier qu'un vélo est disponible"
   },
   {
+    parentDivId : "container-1",
     src: "images/credited/sources/optimise/velib-vert1.png",
     alt: "Vélib sur les quais",
     title : "Vélib' 2018 - Version classique",
-    text : "Cliquer sur réserver puis signer",
-    imgId : "current-slide",
-    txtId : "current-text",
-    parentId : "conteneur"
+    text : "Cliquer sur réserver puis signer"
   },
   {
+    parentDivId : "container-1",
     src: "images/credited/sources/optimise/borne3.png",
     alt: "Vélib sur les quais",
     title : "Velib' 2018 - Nouvelle Borne",
-    text : "Votre vélo est réservé pendant 20 minutes !",
-    imgId : "current-slide",
-    txtId : "current-text",
-    parentId : "conteneur"
+    text : "Votre vélo est réservé pendant 20 minutes !"
   }
 ];
 
-// Constructeur de slide
-var Slide = {
-  init: function (src, alt, title, text, imgId, txtId, parentId) {
+var i = 0; // Valeur du rang de la slide, dans le tableau d'objets "slides"
+
+// Objet Slider
+// et méthodes : position, buttons, keysListener,
+//               slide, removeCurrentSlide, createNewSlide,
+//               previousSlide et nextSlide
+var Slider = {
+  // Définit l'ID du slider. Ajoute l'enfant DIV (contenant la slide active) à l'ID parent
+  position : function (divId, parentId) {
+    var divElt = document.createElement("div");
+    divElt.id = divId;
+    this.parentId = document.getElementById(parentId);
+    this.parentId.appendChild(divElt);
+  },
+  // Définition des boutons de navigation du slider
+  buttons : function (parentId, previousButtonId, nextButtonId) {
+    var previousButton = document.createElement("button");
+    var nextButton = document.createElement("button");
+    previousButton.textContent = " < ";
+    nextButton.textContent = " > ";
+    previousButton.id = previousButtonId;
+    nextButton.id = nextButtonId;
+    // Mise en place des listeners
+    previousButton.addEventListener("click", function () {
+      Slider.previousSlide()
+      clearInterval(animSlider);
+    });
+    nextButton.addEventListener("click", function () {
+      Slider.nextSlide();
+      clearInterval(animSlider);
+    });
+    // Ajout des boutons
+    this.parentId = document.getElementById(parentId);
+    this.parentId.appendChild(previousButton);
+    this.parentId.appendChild(nextButton);
+  },
+  // Mise en place d'un listener sur le clavier
+  keysListener : function () {
+    document.addEventListener("keydown", function (e) {
+      // Règles pour les touches flèche gauche et 4 du pavé numérique
+      if (e.keyCode === 37 || e.keyCode === 100 ) {
+        Slider.previousSlide();
+        clearInterval(animSlider);
+        // Règles pour les touches flèche droite et 6 du pavé numérique
+      } else if (e.keyCode === 39 || e.keyCode === 102 ) {
+        Slider.nextSlide();
+        clearInterval(animSlider);
+      } else {
+        console.log("La touche " + e.keyCode + " n'est pas prise en compte");
+      };
+    });
+  },
+  // Construction d'une slide
+  slide : function (parentId, src, alt, title, text) {
     var imgElt = document.createElement("img");
     var txtElt = document.createElement("h2");
-
     imgElt.src = src;
     imgElt.alt = alt;
     imgElt.title = title;
     txtElt.textContent = text;
-    imgElt.id = imgId;
-    txtElt.id = txtId;
-
     var idElt = document.getElementById(parentId);
     idElt.appendChild(imgElt);
     idElt.appendChild(txtElt);
   },
+  // Suppression de la slide actuelle
+  removeCurrentSlide : function () {
+    var idElt = document.getElementById(slides[i].parentDivId);
+    idElt.innerHTML = "";
+  },
+  // Définition de la nouvelle slide
+  createNewSlide : function () {
+    Slider.slide(
+      slides[i].parentDivId,
+      slides[i].src,
+      slides[i].alt,
+      slides[i].title,
+      slides[i].text
+    )
+  },
+  // Appel de la slide suivante
+  nextSlide : function () {
+    i ++;
+    if (i < slides.length) {
+      Slider.removeCurrentSlide();
+      Slider.createNewSlide(i);
+    } else {
+      i = 0; // Retour à la slide n°1
+      Slider.removeCurrentSlide();
+      Slider.createNewSlide(i);
+    };
+  },
+  // Appel de la slide précédente
+  previousSlide : function () {
+    i --;
+    if (i >= 0) {
+      Slider.removeCurrentSlide();
+      Slider.createNewSlide(i);
+    } else {
+      i = slides.length - 1; // Saut vers la dernière slide
+      Slider.removeCurrentSlide();
+      Slider.createNewSlide(i);
+    };
+  }
 };
 
-// Création de la slide avec l'argument number
-function createNewSlide (number) {
-  var slide = new Slide.init(
-    slides[number].src,
-    slides[number].alt,
-    slides[number].title,
-    slides[number].text,
-    slides[number].imgId,
-    slides[number].txtId,
-    slides[number].parentId
-  );
-};
-
-// Suppression de la slide actuelle
-function removeCurrentSlide () {
-  var idElt = document.getElementById("current-slide");
-  var txtIdElt = document.getElementById("current-text");
-  idElt.parentNode.removeChild(idElt);
-  txtIdElt.parentNode.removeChild(txtIdElt);
-};
-
-// Slide par défaut => premier objet du tableau slides
-var slideNumber = 0;
-// Nouvelle variable pour palier l'oubli de slideNumber (erreur undefined dans les EventListeners)
-var numberArg = slideNumber;
-
-createNewSlide(numberArg);
-
-// Variables des boutons du slider
-var boutonPrevious = document.getElementById("previous");
-var boutonNext = document.getElementById("next");
-
-// Fonctions du slider
-function nextSlide () {
-  numberArg ++;
-  if (numberArg < slides.length) {
-    removeCurrentSlide();
-    createNewSlide(numberArg);
-  } else {
-    numberArg = 0; // Retour à la slide n°1
-    removeCurrentSlide();
-    createNewSlide(numberArg);
-  };
-};
-
-function previousSlide() {
-  numberArg --;
-  if (numberArg >= 0) {
-    removeCurrentSlide();
-    createNewSlide(numberArg);
-  } else {
-    numberArg = slides.length - 1; // Saut vers la dernière slide
-    removeCurrentSlide();
-    createNewSlide(numberArg);
-  };
-};
-
-// Ecoute des boutons
-boutonNext.addEventListener("click", function () {
-  nextSlide();
-  clearInterval(animSlider);
-});
-
-boutonPrevious.addEventListener("click", function () {
-  previousSlide();
-  clearInterval(animSlider);
-});
-
-// Ecoute des touches '< >' et '4 6' du clavier
-document.addEventListener("keydown", function (e) {
-  if (e.keyCode === 37 || e.keyCode === 100 ) {
-    previousSlide();
-    clearInterval(animSlider);
-  } else if (e.keyCode === 39 || e.keyCode === 102 ) {
-    nextSlide();
-    clearInterval(animSlider);
-  } else {
-    console.log("La touche " + e.keyCode + " n'est pas prise en compte");
-  };
-});
-
+// Positionnement du slider
+new Slider.position(slides[i].parentDivId, "slider-1");
+// Définition des boutons
+Slider.buttons("slider-1", "previous-button-1", "next-button-1");
+// Activation de l'écoute du clavier
+Slider.keysListener();
+// Définition de la première slide
+Slider.slide(
+  slides[i].parentDivId,
+  slides[i].src,
+  slides[i].alt,
+  slides[i].title,
+  slides[i].text
+);
 // Animation du slider au chargment de la page (désactivée après commande manuelle)
-var animSlider = setInterval(function() { nextSlide();  },  5000);
+var animSlider = setInterval(function() { Slider.nextSlide();  },  5000);
